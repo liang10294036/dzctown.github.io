8893d5b3dca9aa3a658a8ee1d33ab737f312bf04
dzctown <38022914+dzctown@users.noreply.github.com>
2018-04-03 10:16:28 +0800

    Add files via upload

---
+++ js/default.js
@@ -0,0 +1,9 @@
+	function show() {
+		  		// body...
+		 document.getElementById("maun_event").style.display = "block";
+		  	}
+
+	function hide() {
+		  		// body...
+		document.getElementById("maun_event").style.display = "none";
+		  	}
